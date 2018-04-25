5a6d2195d744df092ea7360e5b96baaa07e3846b
admin admin <evaladmin@example.com>
2018-04-25 18:28:27 -0400

    Wrote content /scripts/pages/entry.groovy

---
+++ scripts/pages/entry.groovy
@@ -0,0 +1,18 @@
+@Grab(group='com.github.jknack', module='handlebars', version='4.0.5')
+
+import com.github.jknack.handlebars.Handlebars
+import com.github.jknack.handlebars.Template
+import com.github.jknack.handlebars.Context
+import com.github.jknack.handlebars.io.TemplateLoader
+import com.github.jknack.handlebars.io.FileTemplateLoader
+import com.github.jknack.handlebars.Context;
+
+def TemplateLoader loader = new FileTemplateLoader("/templates", ".hbs");
+def Handlebars= new Handlebars(loader)
+    
+def template = handlebars.compile(templateName);
+def context = Context.newBuilder([firstName: "russ", lastName: "danner"])
+def output = template.apply(context)
+
+
+templateModel.output = output
