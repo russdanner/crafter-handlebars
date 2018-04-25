0aab9880100fa4982a6aab8eab99e78622b39daf
admin admin <evaladmin@example.com>
2018-04-25 19:11:34 -0400

    Wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -7,11 +7,16 @@ import com.github.jknack.handlebars.io.TemplateLoader
 import com.github.jknack.handlebars.io.FileTemplateLoader
 import com.github.jknack.handlebars.Context
 
+
 def loader = new FileTemplateLoader("/Users/rdanner/crafter-installs/ent/craftercms/crafter-authoring/data/repos/sites/handlebars/sandbox/templates", ".hbs");
 def handlebars = new Handlebars(loader)
     
 def template = handlebars.compile("test");
-def handlebarsModel = [firstName: "russ", lastName: "danner", contentModel: contentModel]
+
+def hbContentModel = [:]
+hbContentModel = contentModel.queryValue("test").text
+
+def handlebarsModel = [firstName: "russ", lastName: "danner", contentModel: hbContentModel]
 def output = template.apply(handlebarsModel)
 
 
