763eb9f4a94fa46e239f337b686ff83e8c191988
admin admin <evaladmin@example.com>
2018-04-25 19:33:37 -0400

    Wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -11,8 +11,8 @@ def handlebars = new Handlebars(loader)
 def template = handlebars.compile("test");
 
 def hbContentModel = [:]
-contentModel.dom.selectNodes("//*").each{ -> node
-	hbContentModel.put(node.id, node.text)
+contentModel.dom.selectNodes("//*").each { -> node
+	hbContentModel.put(node.name, node.value)
 }
 
 
