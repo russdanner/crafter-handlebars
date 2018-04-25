bac086d9b43cc667851100e3fac248598a978097
admin admin <evaladmin@example.com>
2018-04-25 18:43:11 -0400

    Wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -7,7 +7,7 @@ import com.github.jknack.handlebars.io.TemplateLoader
 import com.github.jknack.handlebars.io.FileTemplateLoader
 import com.github.jknack.handlebars.Context;
 
-def loader = new FileTemplateLoader(siteContext.getTemplatesPath(), ".hbs");
+def loader = new FileTemplateLoader(siteContext.getStaticAssetsPath(), ".hbs");
 def handlebars = new Handlebars(loader)
     
 def template = handlebars.compile("test");
