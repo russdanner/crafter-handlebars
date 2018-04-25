64b78ae2baaca6e2e292988d8b90cb2528f49b20
admin admin <evaladmin@example.com>
2018-04-25 18:49:49 -0400

    Wrote content /scripts/pages/entry.groovy

--- scripts/pages/entry.groovy
+++ scripts/pages/entry.groovy
@@ -7,7 +7,7 @@ import com.github.jknack.handlebars.io.TemplateLoader
 import com.github.jknack.handlebars.io.FileTemplateLoader
 import com.github.jknack.handlebars.Context;
 
-def loader = new FileTemplateLoader(siteContext.getStaticAssetsPath(), ".hbs");
+def loader = new FileTemplateLoader("/Users/rdanner/crafter-installs/ent/craftercms/crafter-authoring/data/repos/sites/handlebars/sandbox/templates", ".hbs");
 def handlebars = new Handlebars(loader)
     
 def template = handlebars.compile("test");
